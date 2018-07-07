4649f7f4ae12b856852039c1ad2567634e36dc12
natalynanda <natalynanda@gmail.com>
2018-07-06 20:56:34 -0400

    fix conflicts

--- src/main/java/com/example/simpleWebApplicationExampleMaven/HelloController.java
+++ src/main/java/com/example/simpleWebApplicationExampleMaven/HelloController.java
@@ -5,22 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot Third messsage!";
     }
-
->>>>>>> 956292e... third message
-=======
-
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
-
->>>>>>> 9b7b52f... html template
 }
